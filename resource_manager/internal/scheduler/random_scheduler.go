2a22e850317d1a8e3a17c7d54cd51a294a6d0d49
Faramarz Qoshchi <faramarz.ghooshchi@gmail.com>
2023-09-22 07:33:23 +0330

    Completed random scheduler

--- resource_manager/internal/scheduler/random_scheduler.go
+++ resource_manager/internal/scheduler/random_scheduler.go
@@ -2,7 +2,6 @@ package scheduler
 
 import (
 	"context"
-	"fmt"
 	"math/rand"
 	"resource_manager/internal/cluster"
 	"resource_manager/internal/consts"
@@ -24,7 +23,9 @@ type RandomScheduler struct {
 type ScoreExtension struct{}
 
 func NewRandomScheduler() *RandomScheduler {
-	return &RandomScheduler{baseScheduler: newBaseScheduler()}
+	return &RandomScheduler{
+		baseScheduler: newBaseScheduler(),
+	}
 }
 
 func (rs *RandomScheduler) Name() string {
@@ -35,17 +36,16 @@ func (rs *RandomScheduler) factory(_ runtime.Object, _ framework.Handle) (framew
 	return rs, nil
 }
 
+func (rs *RandomScheduler) ScoreExtensions() framework.ScoreExtensions {
+	return &ScoreExtension{}
+}
+
 func (rs *RandomScheduler) Filter(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
 	pod := cluster.BindPod(*p)
 	node := cluster.BindNode(*nodeInfo.Node())
 	var status framework.Code = framework.Success
 
 	status = rs.baseFilter(pod, node)
-
-	// Only active nodes are available
-	if node.Class != consts.ACTIVE_CLASS {
-		status = framework.Unschedulable
-	}
 
 	klog.Info("Filtering Pod: ", pod.Name, " on ", nodeInfo.Node().Name, " : ", status.String())
 	return framework.NewStatus(status)
@@ -54,19 +54,21 @@ func (rs *RandomScheduler) Filter(ctx context.Context, state *framework.CycleSta
 func (rs *RandomScheduler) PreScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, rawNodes []*v1.Node) *framework.Status {
 	pod := cluster.BindPod(*p)
 	var nodes cluster.NodeList
-	for idx, rawNode := range rawNodes {
-		fmt.Println("idx", idx, rawNode.Name)
+	for _, rawNode := range rawNodes {
 		nodes = append(nodes, cluster.BindNode(*rawNode))
 	}
 	var status framework.Code = framework.Success
 
-	// Select a random node
+	// Give random score to every node
 	rand.Seed(time.Now().UnixNano())
 	rs.set("selected_nodes_for_"+pod.Name, []string{
 		nodes[rand.Intn(len(nodes))].Name,
-		nodes[rand.Intn(len(nodes))].Name,
-		nodes[rand.Intn(len(nodes))].Name,
 	})
+
+	var nodesScore = make(map[string]int64)
+	for _, node := range nodes {
+		nodesScore[node.Name] = int64(rand.Intn(int(framework.MaxNodeScore)))
+	}
 
 	klog.Info("PreScoring Pod: ", pod.Name)
 	return framework.NewStatus(status)
@@ -74,23 +76,16 @@ func (rs *RandomScheduler) PreScore(ctx context.Context, state *framework.CycleS
 
 func (rs *RandomScheduler) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
 	pod := cluster.BindPod(*p)
-	// var status framework.Code = framework.Success
 	var score int64
-
-	mostPrioritizedNodes := (rs.get("selected_nodes_for_" + pod.Name)).([]string)
-	fmt.Println(mostPrioritizedNodes)
-	for i := 0; i < len(mostPrioritizedNodes); i++ {
-		if mostPrioritizedNodes[i] == nodeName {
-			score = 20
+	nodesScore := rs.get("nodes_score_for_" + pod.Name).(map[string]int64)
+	for key, value := range nodesScore {
+		if key == nodeName {
+			score = value
 		}
 	}
 
 	klog.Info("Scoring Pod: ", pod.Name, " on ", nodeName, " : ", score)
-	return score, nil
-}
-
-func (rs *ScoreExtension) ScoreExtensions() framework.ScoreExtensions {
-	return rs
+	return score, framework.NewStatus(framework.Success)
 }
 
 func (se *ScoreExtension) NormalizeScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, scores framework.NodeScoreList) *framework.Status {
@@ -98,14 +93,13 @@ func (se *ScoreExtension) NormalizeScore(ctx context.Context, state *framework.C
 	for _, nodeScore := range scores {
 		highest = max(int64(highest), nodeScore.Score)
 	}
-	MaxNodeScore := 20
+
 	for i, nodeScore := range scores {
-		scores[i].Score = nodeScore.Score * int64(MaxNodeScore) / highest
+		scores[i].Score = nodeScore.Score * int64(framework.MaxNodeScore) / highest
 	}
 
-	fmt.Println("Normalized scores: ", scores)
-
-	return nil
+	klog.Info("Normalized scores: ", scores)
+	return framework.NewStatus(framework.Success)
 }
 
 func max(a, b int64) int64 {
